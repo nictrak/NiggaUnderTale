fa85acec37002d5f7579684ae3780f7ee0d46e16
nictrak <narin14175@gmail.com>
2020-05-27 16:24:10 +0700

    menu and start added

--- sources_1/new/machine.v
+++ sources_1/new/machine.v
@@ -21,7 +21,7 @@
 
 
 module Machine(
-    output reg[7:0] nextState,
+    output wire[7:0] nextState,
     output reg[3:0] playerInstructiuon,
     output reg isMove,
     input wire[7:0] state,
@@ -31,13 +31,13 @@ module Machine(
     input wire clk
     );
     
+    parameter ZERO = 4'b0000; // for IDLE
     //key enum
-    parameter IDLE = 4'b0000;
-    parameter UP = 4'b0001;
-    parameter LEFT = 4'b0010;
-    parameter DOWN = 4'b0011;
-    parameter RIGHT = 4'b0100;
-    parameter ENTER = 4'b0101;
+    parameter W = 4'b0001;
+    parameter D = 4'b0010;
+    parameter S = 4'b0011;
+    parameter A = 4'b0100;
+    parameter SPACE = 4'b0101;
     
     //page enum
     parameter NULL = 4'b0000; //blank screen do nothing
@@ -47,12 +47,42 @@ module Machine(
     parameter ATTACK = 4'b1010; //attack monster state
     parameter ACTION = 4'b1011; //choose action state
     
+    //op code
+    parameter HPY = 4'b0001;
+    parameter DPY = 4'b0010;
+    parameter IDG = 4'b0011;
+    parameter SDG = 4'b0100;
+    parameter MOV = 4'b0101;
+    parameter SHP = 4'b0110;
+    
     wire[3:0] page;
     wire[3:0] substage;
-    assign keyboard = state[7:4];
-    assign menuInp = state[3:0];
-    assign page = 0;
+    assign page = state[7:4];
+    assign substage = state[3:0];
     
+    reg[3:0] nextPage;
+    reg[3:0] nextSubstage;
+    assign nextStage = {nextPage, nextSubstage};
     
+    initial begin
+        nextPage = ZERO;
+        nextSubstage = ZERO;
+    end
+    
+    always @(posedge clk) begin
+        nextPage = page;
+        nextSubstage = substage;
+        case(page)
+            MENU: begin
+                if(keyboard === SPACE) begin
+                    nextPage = START;
+                    nextSubstage = ZERO;
+                end
+            end
+            START: begin
+            end
+        endcase
+    end    
+   
     
 endmodule
